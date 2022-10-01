6e95047d9811df6bf7d8ef93aa75a1387f657350
g0thb0iclique <test@example.com>
2022-10-01 15:56:57 +0300

    Rewrote png2ico.py. Added auto resize for input .PNG files (-r). I.e. if you have only one .PNG file, with -r flag in output you get .ICO file with 7 different sizes (from 16x16 to 255x255).

--- png2ico.py
+++ png2ico.py
@@ -1,28 +1,43 @@
 from pathlib import Path
+from typing import Sequence
 from PIL import Image
 import argparse
 
 
-def png2ico(pngs, ico):
-    data = bytes((0, 0, 1, 0, len(pngs), 0))
-    offset = 6 + len(pngs) * 16
+AUTO_RESIZE_OUTPUT_ICO_SIZES = ((16, 16), (24, 24), (32, 32), (48, 48), (64, 64), (128, 128), (255, 255))
 
-    for png in pngs:
-        img = Image.open(png)
-        data += bytes((img.width, img.height, 0, 0, 1, 0, 32, 0, ))
-        bytesize = Path(png).stat().st_size
-        data += bytesize.to_bytes(4, byteorder="little")
-        data += offset.to_bytes(4, byteorder="little")
-        offset += bytesize
 
-    for png in pngs:
-        data += Path(png).read_bytes()
+def png2ico(pngs_paths: Sequence[str], ico_path: str, *, auto_resize: bool = False):
+    if auto_resize:
+        imgs = {Path(png_path).stat().st_size: png_path for png_path in pngs_paths}
+        largest_img_path = imgs[max(imgs.keys())]
+        img = Image.open(largest_img_path)
+        img.save(ico_path, sizes=AUTO_RESIZE_OUTPUT_ICO_SIZES)
+    else:
+        data = bytes((0, 0, 1, 0, len(pngs_paths), 0))
+        offset = 6 + len(pngs_paths) * 16
 
-    Path(ico).write_bytes(data)
+        for png_path in pngs_paths:
+            img = Image.open(png_path)
+            data += bytes((img.width, img.height, 0, 0, 1, 0, 32, 0,))
+            bytesize = Path(png_path).stat().st_size
+            data += bytesize.to_bytes(4, byteorder="little")
+            data += offset.to_bytes(4, byteorder="little")
+            offset += bytesize
+
+        for png in pngs_paths:
+            data += Path(png).read_bytes()
+
+        Path(ico_path).write_bytes(data)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Converting multiple .PNG files to one multisize .ICO file')
-    parser.add_argument('files', nargs='+', type=str, help='Input .PNG files')
-    parser.add_argument('-o', '--output', default='icon.ico', type=str, help='Output .ICO file')
+    parser.add_argument('files', nargs='+', type=str, help='Input .PNG filenames')
+    parser.add_argument('-o', '--output', default='icon.ico', type=str, required=False, help='Output .ICO filename')
+    parser.add_argument('-r', '--resize', default=False, type=bool, action=argparse.BooleanOptionalAction,
+                        help='Resizes the largest .PNG file automatically. '
+                             'The output is multisize .ICO file with sizes: '
+                             '16x16, 24x24, 32x32, 48x48, 64x64, 128x128, 255x255.')
     args = parser.parse_args()
-    png2ico(args.files, args.output)
+    png2ico(pngs_paths=args.files, ico_path=args.output, auto_resize=args.resize)
